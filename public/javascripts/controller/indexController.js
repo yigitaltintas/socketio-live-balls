1af120e92fe90b300d1ba03c3ec59918398981db
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-12-07 13:47:42 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,29 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
-    const connectionOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 500
+    $scope.init = () => {
+      const username = prompt('Please enter username');
+
+        if(username)
+            initSocket(username);
+        else
+            return false;
     };
 
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-        .then( (socket) => {
-        console.log('baglanti gerceklesti', socket);
-    }).catch((err) => {
-        console.log('hata', err);
-    });
+    function initSocket(username){
+        const connectionOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 500
+        };
+
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+            .then( (socket) => {
+                console.log('baglanti gerceklesti', socket);
+
+                socket.emit('newUser', { username });
+
+
+            }).catch((err) => {
+            console.log('hata', err);
+        });
+    }
 }]);
